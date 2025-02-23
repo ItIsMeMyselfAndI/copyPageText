d2886005cb6abb2a1553c7ed565fe44b86395bdb
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-02-23 21:35:37 +0800

    added user inputs

--- copyRaw.py
+++ copyRaw.py
@@ -8,8 +8,24 @@ import os
 path = ".\\SLR"
 os.makedirs(path, exist_ok=True)
 
-for i in range(180, 308):
-    url = f"https://www.fortuneeternal.com/novel/solo-leveling-ragnarok-raw-novel/chapter-{i}/"
+url = input("Enter URL: ")
+print("What is your starting chapter number?")
+chap_lower = int(float(input("\tEnter integer: ")))
+print("What is your ending chapter number?")
+chap_upper = int(float(input("\tEnter integer: ")))
+print("How many lines from the top of the page do you want to exclude? ")
+page_lower = int(float(input("\tEnter integer: ")))
+print("How many lines from the bottom of the page do you want to exclude? ")
+page_upper = int(float(input("\tEnter integer: "))) * -1
+print()
+
+# url = f"https://www.fortuneeternal.com/novel/solo-leveling-ragnarok-raw-novel/chapter-"
+# chap_lower = 169
+# chap_upper = 308
+# page_lower = 728
+# page_upper = -382
+for i in range(chap_lower, chap_upper + 1):
+    url = f"{url}{i}/"
 
     # Fetch the page content
     response = requests.get(url)
@@ -21,7 +37,7 @@ for i in range(180, 308):
         # pyperclip.copy(text)
         # print("Webpage text copied to clipboard!")
         page = page.split("\n")
-        text = [line for line in page if line != ""][728:-382]
+        text = [line for line in page if line != ""][page_lower:page_upper]
         # for j, line in enumerate(text):
         #     print(j, line)
         output = f"{path}\\{i-1}.docx"
@@ -32,4 +48,5 @@ for i in range(180, 308):
         print(f"[*] Copied [Chapter {i-1}] successfully.")
 
     else:
-        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        print(f"[*]Failed to retrieve the page. Status code: {response.status_code}")
+        print(f"[*]You might want to modify the script or the URL is invalid.")
