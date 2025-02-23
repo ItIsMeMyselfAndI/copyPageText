7db3425e30894f5e39749174413d0cd868226171
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-02-23 21:37:58 +0800

    added a comment for url structure

--- copyRaw.py
+++ copyRaw.py
@@ -5,10 +5,7 @@ import pyperclip  # To copy text to clipboard
 import os
 
 # URL of the webpage
-path = ".\\SLR"
-os.makedirs(path, exist_ok=True)
-
-url = input("Enter URL: ")
+page_url = input("Enter URL: ")
 print("What is your starting chapter number?")
 chap_lower = int(float(input("\tEnter integer: ")))
 print("What is your ending chapter number?")
@@ -17,6 +14,8 @@ print("How many lines from the top of the page do you want to exclude? ")
 page_lower = int(float(input("\tEnter integer: ")))
 print("How many lines from the bottom of the page do you want to exclude? ")
 page_upper = int(float(input("\tEnter integer: "))) * -1
+print("What name you want your output folder to have? ")
+path = input("\tEnter string: ")
 print()
 
 # url = f"https://www.fortuneeternal.com/novel/solo-leveling-ragnarok-raw-novel/chapter-"
@@ -24,22 +23,21 @@ print()
 # chap_upper = 308
 # page_lower = 728
 # page_upper = -382
+# path = ".\\SLR"
+
+os.makedirs(path, exist_ok=True)
 for i in range(chap_lower, chap_upper + 1):
-    url = f"{url}{i}/"
+    # edit this part for specifi websites
+    chap_url = f"{page_url}{i}/"
 
     # Fetch the page content
-    response = requests.get(url)
+    response = requests.get(chap_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         # Extract all text from the page
         page = soup.get_text()
-        # Copy text to clipboard
-        # pyperclip.copy(text)
-        # print("Webpage text copied to clipboard!")
         page = page.split("\n")
         text = [line for line in page if line != ""][page_lower:page_upper]
-        # for j, line in enumerate(text):
-        #     print(j, line)
         output = f"{path}\\{i-1}.docx"
         document = Document()
         for line in text:
